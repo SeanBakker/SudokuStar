6939d71c5a5a794e2bc9fdc3e02021068e9703e7
Sean Bakker <69602780+SeanBakker@users.noreply.github.com>
2021-02-18 17:56:25 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/sudokustar/game/SudokuGame.kt
@@ -0,0 +1,23 @@
+package com.example.sudokustar.game
+
+import androidx.lifecycle.MutableLiveData
+
+class SudokuGame {
+
+    var selectedCellLiveData = MutableLiveData<Pair<Int, Int>>()
+
+    private var selectedRow = -1
+    private var selectedCol = -1
+
+    init {
+        selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
+    }
+
+    fun updateSelectedCell(row: Int, col: Int) {
+        selectedRow = row
+        selectedCol = col
+        selectedCellLiveData.postValue(Pair(row, col))
+    }
+
+
+}
