9ae18f09c3f08ef99bc9199eb1b57a2710b4c425
Sean Bakker <69602780+SeanBakker@users.noreply.github.com>
2021-02-23 17:29:19 -0500

    Block user input on specified starting cells

--- app/src/main/java/com/example/sudokustar/game/SudokuGame.kt
+++ app/src/main/java/com/example/sudokustar/game/SudokuGame.kt
@@ -14,6 +14,11 @@ class SudokuGame {
 
     init {
         val cells = List(9 * 9) {i -> Cell(i / 9, i % 9, i % 9)}
+
+        //Testing starting cell functionality
+        cells[11].isStartingCell = true
+        cells[21].isStartingCell = true
+
         board = Board(9, cells)
         selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
         cellsLiveData.postValue(board.cells)
@@ -22,14 +27,20 @@ class SudokuGame {
     fun handleInput(number: Int) {
         if (selectedRow == -1 || selectedCol == -1) return
 
+        //Extra check to ensure user can not change a starting cell
+        if (board.getCell(selectedRow, selectedCol).isStartingCell) return
+
         board.getCell(selectedRow, selectedCol).value = number
         cellsLiveData.postValue(board.cells)
     }
 
     fun updateSelectedCell(row: Int, col: Int) {
-        selectedRow = row
-        selectedCol = col
-        selectedCellLiveData.postValue(Pair(row, col))
+        //Starting cells can not be updated, while other cells can take user input
+        if (!board.getCell(row, col).isStartingCell) {
+            selectedRow = row
+            selectedCol = col
+            selectedCellLiveData.postValue(Pair(row, col))
+        }
     }
 
 
