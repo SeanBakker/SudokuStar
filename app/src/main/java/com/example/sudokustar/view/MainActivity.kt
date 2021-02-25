2b350b488f2dc569109023a86b7d59fe6c533194
Sean Bakker <69602780+SeanBakker@users.noreply.github.com>
2021-02-24 19:51:56 -0500

    Added Functions for Note Taking

--- app/src/main/java/com/example/sudokustar/view/MainActivity.kt
+++ app/src/main/java/com/example/sudokustar/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sudokustar.view
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.sudokustar.R
@@ -13,6 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), SudokuStarBoardView.OnTouchListener {
 
     private lateinit var viewModel: SudokuStarViewModel
+    private lateinit var numberButtons: List<Button>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +28,21 @@ class MainActivity : AppCompatActivity(), SudokuStarBoardView.OnTouchListener {
         viewModel = ViewModelProviders.of(this).get(SudokuStarViewModel::class.java)
         viewModel.sudokuGame.selectedCellLiveData.observe(this, Observer { updateSelectedCellUI(it) })
         viewModel.sudokuGame.cellsLiveData.observe(this, Observer { updateCells(it) })
+        viewModel.sudokuGame.isTakingNotesLiveData.observe(this, Observer { updateNoteTakingUI(it) })
+        viewModel.sudokuGame.highlightedKeysLiveData.observe(this, Observer { updateHighlightedKeys(it) })
 
-        val buttons = listOf(oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton)
+        numberButtons = listOf(oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton)
 
         //Read button input
-        buttons.forEachIndexed { index, button ->
+        numberButtons.forEachIndexed { index, button ->
             button.setOnClickListener { viewModel.sudokuGame.handleInput(index + 1) }
         }
+
+        //Call changeNoteTakingState function when notesButton is clicked
+        notesButton.setOnClickListener { viewModel.sudokuGame.changeNoteTakingState() }
+
+        //Call delete function when deleteButton is clicked
+        deleteButton.setOnClickListener { viewModel.sudokuGame.delete() }
     }
 
     private fun updateCells(cells: List<Cell>?) = cells?.let {
@@ -38,6 +51,18 @@ class MainActivity : AppCompatActivity(), SudokuStarBoardView.OnTouchListener {
 
     private fun updateSelectedCellUI(cell: Pair<Int, Int>?) = cell?.let {
         sudokuStarBoardView.updateSelectedCellUI(cell.first, cell.second)
+    }
+
+    private fun updateNoteTakingUI(isNoteTaking: Boolean?) = isNoteTaking?.let {
+        val color = if (it) ContextCompat.getColor(this, R.color.design_default_color_primary) else Color.LTGRAY
+        notesButton.background.setColorFilter(color, PorterDuff.Mode.MULTIPLY)
+    }
+
+    private fun updateHighlightedKeys(set: Set<Int>?) = set?.let {
+        numberButtons.forEachIndexed { index, button ->
+            val color = if (set.contains(index + 1)) ContextCompat.getColor(this, R.color.design_default_color_primary) else Color.LTGRAY
+            button.setBackgroundColor(color)
+        }
     }
 
     override fun onCellTouched(row: Int, col: Int) {
